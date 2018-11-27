2604505709a1c71ebdfe9a69736b5bd9b4cfe138
Sajjad Mohammadi <radhoosh.user2@gmail.com>
2018-11-27 18:58:10 +1100

    config pods file and install

---
+++ ios/Pods/Target Support Files/Pods-babahassan/Pods-babahassan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_babahassan : NSObject
+@end
+@implementation PodsDummy_Pods_babahassan
+@end
---
+++ ios/Pods/Target Support Files/Pods-babahassanTests/Pods-babahassanTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_babahassanTests : NSObject
+@end
+@implementation PodsDummy_Pods_babahassanTests
+@end
